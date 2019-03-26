320886f46751d97c53e6f7265d870daad806651a
Guilherme Faria <guiefaria@gmail.com>
2019-03-25 22:34:53 -0300

    Teste

--- ProjetoTeste_Git/Program.cs
+++ ProjetoTeste_Git/Program.cs
@@ -5,6 +5,7 @@ namespace ProjetoTeste_Git {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Oi");
         }
     }
 }
